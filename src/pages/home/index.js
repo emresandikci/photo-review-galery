4644f92586ed4a63a8d3e95d7a4e9fab5e80da22
emresandikci <me@emresandikci.com>
2021-10-03 16:24:55 +0300

    feat: test home page created

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,29 +1,7 @@
 import React from 'react';
-import { Box, Heading, Text, Button } from 'esducad-ui';
+
 function Home() {
-  return (
-    <Box
-      display="flex"
-      flexDirection="column"
-      height="100vh"
-      justifyContent="center"
-      alignItems="center"
-      bg="primary"
-    >
-      <Heading fontSize="50px" color="white" animation="bounce">
-        React easy starter boilerplate
-      </Heading>
-      <Text color="white" animation="bounce">
-        by emresandikci to see the other projects{' '}
-        <Button
-          variant="secondary"
-          onClick={() => (window.location.href = 'http://github.com/emresandikci')}
-        >
-          click here
-        </Button>
-      </Text>
-    </Box>
-  );
+  return <div>test</div>;
 }
 
 export default Home;
