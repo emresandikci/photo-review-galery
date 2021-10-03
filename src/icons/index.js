a78d724914084387f4088fad24140c7bc7961c33
emresandikci <me@emresandikci.com>
2021-10-03 20:12:23 +0300

    feat(icons): svg icons generated to react component

---
+++ src/icons/index.js
@@ -0,0 +1,4 @@
+export { default as IconArrow } from './IconArrow';
+export { default as IconCheck } from './IconCheck';
+export { default as IconPlus } from './IconPlus';
+export { default as Spinner } from './Spinner';
