baa53a1d95690d690a21615875d70cec5fa30764
emresandikci <me@emresandikci.com>
2021-10-03 16:19:39 +0300

    refactor(app): app starter path refactored

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Home } from 'pages';
+import { GlobalStyle } from 'ui';
+import { Layout } from 'containers';
+import { Seo } from 'utils';
+
+const theme = {
+  colors: {
+    primary: ['#3b55e4', '#354dcd', '#2f44b6'],
+    secondary: ['#454545', '#3e3e3e', '#373737'],
+    background: '#FEFFFE',
+    text: '#A4A4A5',
+    white: '#fff',
+    black: '#454445',
+    gray: '#F1F3F7',
+    darkGray: '#E2E9F0',
+  },
+  fontFamily: "'Roboto', sans-serif;",
+};
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Seo />
+      <Router>
+        <Layout>
+          <Switch>
+            <Route path="/" component={Home} exact />
+          </Switch>
+        </Layout>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+render(<App />, document.getElementById('app'));
