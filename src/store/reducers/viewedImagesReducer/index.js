3d7a543e6b7c5c9c7ed6c654a80cbfb1f0b20e36
emresandikci <me@emresandikci.com>
2021-10-09 06:01:57 +0300

    feat: remove image types and reducer added

--- src/store/reducers/viewedImagesReducer/index.js
+++ src/store/reducers/viewedImagesReducer/index.js
@@ -5,10 +5,13 @@ import {
   SET_VIEWED_IMAGES_STARTED,
   SET_VIEWED_IMAGES_SUCCEDED,
   SET_VIEWED_IMAGES_FAILED,
+  DELETE_VIEWED_IMAGES_STARTED,
+  DELETE_VIEWED_IMAGES_SUCCEDED,
+  DELETE_VIEWED_IMAGES_FAILED,
 } from 'store/types/viewedImages';
 
 const initialState = {
-  data: [], //[{ url: '', isApproved: false, isRejected: false }],
+  data: [],
   isLoading: false,
   error: null,
 };
@@ -49,6 +52,26 @@ const reducer = (state = initialState, { type, payload }) => {
       };
 
     case SET_VIEWED_IMAGES_FAILED:
+      return {
+        ...state,
+        data: null,
+        isLoading: false,
+        error: payload,
+      };
+    case DELETE_VIEWED_IMAGES_STARTED:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    case DELETE_VIEWED_IMAGES_SUCCEDED:
+      return {
+        ...state,
+        data: state.data?.filter((f) => f.id !== payload),
+        isLoading: false,
+      };
+
+    case DELETE_VIEWED_IMAGES_FAILED:
       return {
         ...state,
         data: null,
