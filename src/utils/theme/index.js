d4173cb877af8f7f653876b70c85538d111e0875
emresandikci <me@emresandikci.com>
2021-10-09 05:53:50 +0300

    feat: app theme added

---
+++ src/utils/theme/index.js
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    primary: ['#3b55e4', '#354dcd', '#2f44b6'],
+    secondary: ['#454545', '#3e3e3e', '#373737'],
+    background: '#FEFFFE',
+    text: '#A4A4A5',
+    white: '#fff',
+    black: '#454445',
+    gray: '#F1F3F7',
+    darkGray: '#E2E9F0',
+  },
+  fontFamily: "'Roboto', sans-serif;",
+};
