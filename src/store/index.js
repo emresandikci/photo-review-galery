11a9b1c16def381bf30fbde5c048fe8ce2982a69
emresandikci <me@emresandikci.com>
2021-10-09 05:50:51 +0300

    feat: redux-persist added

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,21 @@
 import { createStore } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import reducers from './reducers/index';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const store = createStore(
-  reducers,
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+let store = createStore(
+  persistedReducer,
   !isProduction && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+let persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
