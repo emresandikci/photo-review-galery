dcf7db2ac088734b76ebecf3d61d2a999bb5e299
emresandikci <me@emresandikci.com>
2021-10-09 05:54:45 +0300

    refactor: redux persist and main layout added

--- src/app.js
+++ src/app.js
@@ -4,39 +4,28 @@ import { ThemeProvider } from 'styled-components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Home } from 'pages';
 import { GlobalStyle } from 'ui';
-import { Layout } from 'containers';
-import { Seo } from 'utils';
+import { Main } from 'layouts';
+import { Seo, theme } from 'utils';
 import { Provider } from 'react-redux';
-import store from 'store';
-
-const theme = {
-  colors: {
-    primary: ['#3b55e4', '#354dcd', '#2f44b6'],
-    secondary: ['#454545', '#3e3e3e', '#373737'],
-    background: '#FEFFFE',
-    text: '#A4A4A5',
-    white: '#fff',
-    black: '#454445',
-    gray: '#F1F3F7',
-    darkGray: '#E2E9F0',
-  },
-  fontFamily: "'Roboto', sans-serif;",
-};
+import globalStore from 'store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 function App() {
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Seo />
-        <Router>
-          <Layout>
-            <Switch>
-              <Route path="/" component={Home} exact />
-            </Switch>
-          </Layout>
-        </Router>
-      </ThemeProvider>
+    <Provider store={globalStore.store}>
+      <PersistGate loading={null} persistor={globalStore.persistor}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Seo />
+          <Router>
+            <Main>
+              <Switch>
+                <Route path="/" component={Home} exact />
+              </Switch>
+            </Main>
+          </Router>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
