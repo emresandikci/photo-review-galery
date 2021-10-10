c7628f1027fb753464e15911e422c001f6b9e248
emresandikci <me@emresandikci.com>
2021-10-10 23:55:18 +0300

    feat(redux-thunk): setup completed

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import reducers from './reducers/index';
+import thunk from 'redux-thunk';
 
 const isProduction = process.env.NODE_ENV === 'production';
 
@@ -12,10 +13,12 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 
-let store = createStore(
-  persistedReducer,
-  !isProduction && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const reduxDevtool =
+  !isProduction && window.__REDUX_DEVTOOLS_EXTENSION__
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : (f) => f;
+
+let store = createStore(persistedReducer, compose(applyMiddleware(thunk), reduxDevtool));
 let persistor = persistStore(store);
 
 export default { store, persistor };
